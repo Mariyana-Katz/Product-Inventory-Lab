5d3e60395a2140f6538139514547316551b19035
Mariyana-Katz <mariqnak21@gmail.com>
2024-04-19 11:16:50 -0400

    some variables added

--- src/main/java/models/Cake.java
+++ src/main/java/models/Cake.java
@@ -1,4 +1,10 @@
 package models;
 
 public class Cake {
+    private int id;
+    private String name;
+    private String filling;
+    private String icing;
+    private int size;
+    private float price;
 }
--- src/main/java/models/Macarons.java
+++ src/main/java/models/Macarons.java
@@ -1,4 +1,9 @@
 package models;
 
 public class Macarons {
+    private int id;
+    private String name;
+    private String filling;
+    private int qty;
+    private float price;
 }
