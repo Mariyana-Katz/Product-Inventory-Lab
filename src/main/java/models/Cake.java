58b0fb627dfb7ccd3bb1b24ec1a0ce37cb4cba64
Mariyana-Katz <mariqnak21@gmail.com>
2024-04-19 20:59:27 -0400

    some tests done

--- src/main/java/models/Cake.java
+++ src/main/java/models/Cake.java
@@ -7,4 +7,60 @@ public class Cake {
     private String icing;
     private int size;
     private float price;
+
+    public Cake(String name, int id, String filling, int size, float price) {
+        this.name = name;
+        this.id = id;
+        this.filling = filling;
+        this.size = size;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName() {
+        this.name = name;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getFilling() {
+        return filling;
+    }
+
+    public void setFilling(String filling) {
+        this.filling = filling;
+    }
+
+    public String getIcing() {
+        return icing;
+    }
+
+    public void setIcing(String icing) {
+        this.icing = icing;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
 }
--- src/test/java/models/CakeTest.java
+++ src/test/java/models/CakeTest.java
@@ -1,4 +1,98 @@
 package models;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class CakeTest {
+    @Test
+    public void Cake(){
+        String expectedName = "Strawberry";
+        int expectedId = 3;
+        String expectedFilling = "Chocolate";
+        int expectedSize = 30;
+        float expectedPrice = 45;
+        Cake cake = new Cake(expectedName, expectedId, expectedFilling, expectedSize, expectedPrice );
+
+        Assertions.assertEquals(expectedName, cake.getName());
+        Assertions.assertEquals(expectedId, cake.getId());
+        Assertions.assertEquals(expectedFilling, cake.getFilling());
+        Assertions.assertEquals(expectedSize,cake.getSize());
+        Assertions.assertEquals(expectedPrice,cake.getPrice());
+
+
+
+
+
+
+    }
+
+    @Test
+    public void setNameTest() {
+        // given (1)
+        Cake cake = new Cake("CarrotCake", 1, "butterCream", 20, 40);
+
+
+        // when (2)
+        String expected = "CarrotCake";
+        String actual = cake.getName();
+
+        // then (3)
+        Assertions.assertEquals(expected, actual);
+    }
+   @ Test
+   public void setIdTest() {
+        // given (1)
+        Cake cake = new Cake("CarrotCake", 2, "butterCream", 20, 40);
+
+
+        // when (2)
+        int expected = 2;
+        int actual = cake.getId();
+
+        // then (3)
+        Assertions.assertEquals(expected, actual);
+    }
+    @Test
+
+    public void setFillingTest() {
+        Cake cake = new Cake("CarrotCake", 1, "Vanilla", 20, 40);
+
+
+        // when (2)
+        String expected = "Vanilla";
+        String actual = cake.getFilling();
+
+        // then (3)
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+
+    public void setSizeTest() {
+        Cake cake = new Cake("CarrotCake", 1, "Vanilla", 20, 40);
+
+
+        // when (2)
+        int expected = 20;
+        int actual = cake.getSize();
+
+        // then (3)
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+
+    public void setPriceTest() {
+        Cake cake = new Cake("CarrotCake", 1, "Vanilla", 20, 45);
+
+
+        // when (2)
+        float expected = 45;
+        float actual = cake.getPrice();
+
+        // then (3)
+        Assertions.assertEquals(expected, actual);
+    }
 }
+
+
